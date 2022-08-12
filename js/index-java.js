2e78326a3147587d215097d0a50c13036390444c
Silas Rodrigues <silasnas2018@gmail.com>
2022-08-12 00:25:12 -0300

    Layout - Menu
    Terminado o Layout do menu central.

---
+++ js/index-java.js
@@ -0,0 +1,10 @@
+document.addEventListener("scroll", function() {
+  var posicaoy = window.pageYOffset;
+});
+/*-------^^^^^^^^^^^^^^^^^^^^ Posição do scroll*/
+
+
+/*----  Quando a Página carregar ------------*/
+function esconderInformacoes(){
+
+}
